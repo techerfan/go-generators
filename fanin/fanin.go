43e168c17cc761066cb36ce5887af11bd7dbfeee
Erfan <techerfan@gmail.com>
2021-11-22 12:50:47 +0330

    add fan-in generator

---
+++ fanin/fanin.go
@@ -0,0 +1,33 @@
+package fanin
+
+import (
+	"sync"
+)
+
+func FanIn(done <-chan interface{}, channels ...<-chan interface{}) <-chan interface{} {
+	var wg sync.WaitGroup
+	multiplexedStream := make(chan interface{})
+
+	multiplex := func(c <-chan interface{}) {
+		defer wg.Done()
+		for i := range c {
+			select {
+			case <-done:
+				return
+			case multiplexedStream <- i:
+			}
+		}
+	}
+
+	wg.Add(len(channels))
+	for _, c := range channels {
+		go multiplex(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(multiplexedStream)
+	}()
+
+	return multiplexedStream
+}
